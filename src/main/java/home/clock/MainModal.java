e5214dc0c15065b2f4836abb262083f281064f3c
César García Mauricio <cesar.garcia4.next@bbva.com>
2019-07-01 09:00:15 -0500

    Removing logger, using lombok instance in MainModal.

--- src/main/java/home/clock/MainModal.java
+++ src/main/java/home/clock/MainModal.java
@@ -1,10 +1,5 @@
 package home.clock;
 
-import java.lang.invoke.MethodHandles;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.text.Text;
@@ -21,8 +16,6 @@ import lombok.extern.slf4j.Slf4j;
 @SuppressWarnings({ "PMD.AtLeastOneConstructor", "PMD.DataflowAnomalyAnalysis" })
 @Slf4j
 public class MainModal extends Application {
-    /** Standard Logger. */
-    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     /** Window width in pixels. */
     private static final double WIDTH = 150;
     /** Window height in pixels. */
@@ -59,8 +52,8 @@ public class MainModal extends Application {
     private Scene setupScene() {
         final Scene scene = new Scene(new DigitalClock(), WIDTH, HEIGHT);
         scene.setOnMouseClicked(event -> {
-            if (event.getTarget() instanceof Text && LOGGER.isInfoEnabled()) {
-                LOGGER.info("Event time registered:\n{}", Text.class.cast(event.getTarget()).getText());
+            if (event.getTarget() instanceof Text) {
+                log.info("Event time registered:\n{}", Text.class.cast(event.getTarget()).getText());
             }
         });
         return scene;
